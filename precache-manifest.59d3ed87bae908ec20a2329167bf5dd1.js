4e1d9ca164cd9615bedace7122973f2a2299d576
Graham Otte <go@grahamotte.com>
2020-06-12 08:04:41 -0700

    Updates

---
+++ precache-manifest.59d3ed87bae908ec20a2329167bf5dd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1550e6591c5c3beee32261c6c79536",
+    "url": "/whatahappytree/index.html"
+  },
+  {
+    "revision": "9164c76a7396332156c4",
+    "url": "/whatahappytree/static/css/main.67afa458.chunk.css"
+  },
+  {
+    "revision": "b24182cacceec2c8e14a",
+    "url": "/whatahappytree/static/js/2.2c00aa5a.chunk.js"
+  },
+  {
+    "revision": "fe5140e8f9e310d4360aa0e1d2676d41",
+    "url": "/whatahappytree/static/js/2.2c00aa5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9164c76a7396332156c4",
+    "url": "/whatahappytree/static/js/main.82a98509.chunk.js"
+  },
+  {
+    "revision": "33a08c4f8b967bf554f7",
+    "url": "/whatahappytree/static/js/runtime-main.0e347ab2.js"
+  }
+]);
